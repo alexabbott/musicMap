1c85993a6f71223dd32d4019153dcfb5a68cd2b6
alexabbott <alex.r.abbott@gmail.com>
2016-08-04 21:56:23 -0400

    Pull station data from google sheet and build form to input new stations

--- www/js/app.js
+++ www/js/app.js
@@ -36,7 +36,9 @@ angular.module('starter', ['ionic', 'ngCordova'])
 
 })
 
-.controller('MapCtrl', function($scope, $state, $cordovaGeolocation) {
+.controller('MapCtrl', function($scope, $state, $cordovaGeolocation, $http) {
+
+    var infowindow = new google.maps.InfoWindow({pixelOffset: new google.maps.Size(-25, 0)});
 
     // check if an element exists in array using a comparer function
     // comparer : function(currentElement)
@@ -55,10 +57,9 @@ angular.module('starter', ['ionic', 'ngCordova'])
         }
     };
 
-    var infowindow = new google.maps.InfoWindow();
-    function createMarker(latlng, radius, html){
+    function createMarker(icon, latlng, radius, color, html){
       var image = {
-        url: '../img/dj.png',
+        url: icon,
         size: new google.maps.Size(71, 71),
         origin: new google.maps.Point(0, 0),
         anchor: new google.maps.Point(12, 13),
@@ -72,7 +73,7 @@ angular.module('starter', ['ionic', 'ngCordova'])
       var stationCircle = new google.maps.Circle({
         map: $scope.map,
         radius: radius,
-        fillColor: '#33cd5f'
+        fillColor: color
       });
 
       google.maps.event.addListener(stationMarker, 'click', function() {
@@ -89,33 +90,120 @@ angular.module('starter', ['ionic', 'ngCordova'])
 
     var options = {timeout: 10000, enableHighAccuracy: true};
 
+
     $cordovaGeolocation.getCurrentPosition(options).then(function(position){
-
-      var mapOptions = {
-        center: $scope.latLng,
-        zoom: 15,
-        zoomControl: false,
-        mapTypeControl: false,
-        scaleControl: false,
-        streetViewControl: false,
-        rotateControl: false,
-        fullscreenControl: false,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-      };
-
-
-      $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
       $scope.user = {
         latLng: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
         radius: 404
       }
-      // user marker
-      $scope.marker = new google.maps.Marker({
-          position: $scope.user.latLng,
-          map: $scope.map,
-          draggable: true
-      });
+
+      $scope.stations = [];
+      $http({
+        url: 'https://sheets.googleapis.com/v4/spreadsheets/10w2lO6wz7amKq8taWrsh6qN-bBXHyya46Z-__8TK0Uc/values/Sheet1!A2:M99?key=AIzaSyD9e_lkQIiKtphl0vGK3MjbC589jQcRtvk'
+      }).then(function successCallback(response) {
+
+        $scope.latLng = {lat: 40.748028, lng: -73.997232};
+
+        $scope.mapOptions = {
+            center: $scope.latLng,
+            zoom: 15,
+            zoomControl: false,
+            mapTypeControl: false,
+            scaleControl: false,
+            streetViewControl: false,
+            rotateControl: false,
+            fullscreenControl: false,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+          };
+        $scope.map = new google.maps.Map(document.getElementById("map"), $scope.mapOptions);
+        $scope.map.setTilt(45);
+
+        // user marker
+        $scope.marker = new google.maps.Marker({
+            position: $scope.user.latLng,
+            map: $scope.map,
+            draggable: true,
+            zIndex: 99
+        });
+        $scope.map.panTo($scope.user.latLng);
+
+          for (var n = 0; n < response['data']['values'].length; n++) {
+            $scope.stations[n] = {};
+            $scope.stations[n]['name'] = response['data']['values'][n][1]
+            $scope.stations[n]['experience'] = response['data']['values'][n][2]
+            $scope.stations[n]['featured'] = response['data']['values'][n][3]
+            $scope.stations[n]['color'] = response['data']['values'][n][4]
+            $scope.stations[n]['icon'] = response['data']['values'][n][5]
+            $scope.stations[n]['lat'] = response['data']['values'][n][6]
+            $scope.stations[n]['lng'] = response['data']['values'][n][7]
+            $scope.stations[n]['playing'] = response['data']['values'][n][8]
+            $scope.stations[n]['vibes'] = response['data']['values'][n][9]
+            $scope.stations[n]['stream'] = response['data']['values'][n][10]
+          }
+          $scope.playing = {};
+
+
+          // station markers
+          $scope.topStation = 0;
+          $scope.stationsInRange = [];
+          for (var n = 0; n < $scope.stations.length; n++) {
+            $scope.stations[n].radius = 10 * $scope.stations[n].experience;
+            $scope.stations[n].latLng = new google.maps.LatLng($scope.stations[n].lat, $scope.stations[n].lng);
+
+            if ($scope.stations[n].playing == "TRUE") {
+              if ($scope.stations[n].experience >= $scope.topStation && google.maps.geometry.spherical.computeDistanceBetween($scope.user.latLng, $scope.stations[n].latLng) < $scope.stations[n].radius) {
+                $scope.topStation = $scope.stations[n].experience;
+              }
+              createMarker($scope.stations[n].icon, $scope.stations[n].latLng, $scope.stations[n].radius, $scope.stations[n].color, $scope.stations[n].name);
+              if (google.maps.geometry.spherical.computeDistanceBetween($scope.user.latLng, $scope.stations[n].latLng) < $scope.stations[n].radius && $scope.stations[n].playing == "TRUE" && $scope.stations[n].experience >= $scope.topStation) {
+                document.getElementById('player').innerHTML = $scope.stations[n].stream;
+                $scope.playing = $scope.stations[n];
+              }
+              if (google.maps.geometry.spherical.computeDistanceBetween($scope.user.latLng, $scope.stations[n].latLng) < $scope.stations[n].radius && $scope.stations[n].playing == "TRUE") {
+                $scope.stationsInRange.pushIfNotExist($scope.stations[n], function(e) {
+                    return e.name === $scope.stations[n].name;
+                });
+              }
+            }
+          }
+
+          setInterval(function() {
+            $scope.user.latLng = $scope.marker.position;
+            // $cordovaGeolocation.getCurrentPosition(options);
+            // $scope.user.latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+            // console.log('lat: ' + position.coords.latitude + ' | lng: ' + position.coords.longitude);
+            // $scope.marker.setPosition({lat: 23.323, lng: -23.344});
+            // $scope.map.panTo({lat: 23.323, lng: -23.344});
+            // $scope.marker.setPosition($scope.user.latLng);
+            // $scope.map.panTo($scope.user.latLng);
+
+
+
+            for (var n = 0; n < $scope.stations.length; n++) {
+              if (google.maps.geometry.spherical.computeDistanceBetween($scope.user.latLng, $scope.stations[n].latLng) < $scope.stations[n].radius && $scope.stations[n].playing == "TRUE") {
+                $scope.stationsInRange.pushIfNotExist($scope.stations[n], function(e) {
+                    return e.name === $scope.stations[n].name;
+                });
+              } else if (google.maps.geometry.spherical.computeDistanceBetween($scope.user.latLng, $scope.stations[n].latLng) > $scope.stations[n].radius && $scope.stations[n].playing == "TRUE") {
+                var result = $scope.stationsInRange.filter(function(obj) {
+                  return obj.name !== $scope.stations[n].name
+                });
+                $scope.stationsInRange = result;
+              }
+            }
+          }, 2000);
+        }, function errorCallback(response) {
+          console.log(response);
+        });
+
+      function setFormPosition() {
+        document.getElementById('input-lat').value = position.coords.latitude;
+        document.getElementById('input-lng').value = position.coords.longitude;
+      }
+      setFormPosition();
+
+
       // $scope.circle = new google.maps.Circle({
       //   map: $scope.map,
       //   radius: $scope.user.radius,
@@ -123,87 +211,45 @@ angular.module('starter', ['ionic', 'ngCordova'])
       // });
       // $scope.circle.bindTo('center', $scope.marker, 'position');
 
-      $scope.stations = [
-        {
-          experience: 45,
-          name: 'djank yucca',
-          lat: 40.730923,
-          lng: -73.997232,
-          playing: true,
-          vibes: 'jungle',
-          song: '<iframe width="100%" height="100" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/218081688&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>'
-        },
-        {
-          experience: 10,
-          name: 'albeit',
-          lat: 40.748028,
-          lng: -73.995755,
-          playing: true,
-          vibes: 'chill',
-          song: '<iframe width="100%" height="100" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/184815214&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>'
-        },
-        {
-          experience: 30,
-          name: '3lo',
-          lat: 40.730823,
-          lng: -73.997332,
-          playing: true,
-          vibes: 'hip hop',
-          song: '<iframe width="100%" height="100" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/219432939&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>'
-        }
-      ];
+      // $scope.stations = [
+      //   {
+      //     name: 'djank yucca',
+      //     experience: 45,
+      //     featured: true,
+      //     color: '#33cd5f',
+      //     icon: '../img/dj.png',
+      //     lat: 40.730923,
+      //     lng: -73.997232,
+      //     playing: true,
+      //     vibes: 'jungle',
+      //     stream: '<iframe width="100%" height="100" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/218081688&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>'
+      //   },
+      //   {
+      //     name: 'albeit',
+      //     experience: 10,
+      //     featured: true,
+      //     color: '#33cd5f',
+      //     icon: '../img/dj.png',
+      //     lat: 40.748028,
+      //     lng: -73.995755,
+      //     playing: true,
+      //     vibes: 'chill',
+      //     stream: '<iframe width="100%" height="100" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/184815214&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>'
+      //   },
+      //   {
+      //     name: '3lo',
+      //     experience: 30,
+      //     featured: true,
+      //     color: '#33cd5f',
+      //     icon: '../img/dj.png',
+      //     lat: 40.750823,
+      //     lng: -73.987332,
+      //     playing: true,
+      //     vibes: 'hip hop',
+      //     stream: '<iframe width="100%" height="100" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/219432939&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>'
+      //   }
+      // ];
 
-      $scope.playing = {};
-
-      // station markers
-      $scope.topStation = 0;
-      $scope.stationsInRange = [];
-      for (var n = 0; n < $scope.stations.length; n++) {
-        $scope.stations[n].radius = 10 * $scope.stations[n].experience;
-        $scope.stations[n].latLng = new google.maps.LatLng($scope.stations[n].lat, $scope.stations[n].lng);
-
-        if ($scope.stations[n].playing === true) {
-          if ($scope.stations[n].experience >= $scope.topStation && google.maps.geometry.spherical.computeDistanceBetween($scope.user.latLng, $scope.stations[n].latLng) < $scope.stations[n].radius) {
-            $scope.topStation = $scope.stations[n].experience;
-          }
-          createMarker($scope.stations[n].latLng, $scope.stations[n].radius, $scope.stations[n].name);
-          if (google.maps.geometry.spherical.computeDistanceBetween($scope.user.latLng, $scope.stations[n].latLng) < $scope.stations[n].radius && $scope.stations[n].playing === true && $scope.stations[n].experience >= $scope.topStation) {
-            document.getElementById('player').innerHTML = $scope.stations[n].song;
-            $scope.playing = $scope.stations[n];
-          }
-          if (google.maps.geometry.spherical.computeDistanceBetween($scope.user.latLng, $scope.stations[n].latLng) < $scope.stations[n].radius && $scope.stations[n].playing === true) {
-            $scope.stationsInRange.pushIfNotExist($scope.stations[n], function(e) {
-                return e.name === $scope.stations[n].name;
-            });
-          }
-        }
-      }
-
-      $scope.map.panTo($scope.user.latLng);
-
-      setInterval(function() {
-        $scope.user.latLng = $scope.marker.position;
-        // $cordovaGeolocation.getCurrentPosition(options);
-        // $scope.user.latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-        // console.log('lat: ' + position.coords.latitude + ' | lng: ' + position.coords.longitude);
-        // $scope.marker.setPosition({lat: 23.323, lng: -23.344});
-        // $scope.map.panTo({lat: 23.323, lng: -23.344});
-        // $scope.marker.setPosition($scope.user.latLng);
-        // $scope.map.panTo($scope.user.latLng);
-
-        for (var n = 0; n < $scope.stations.length; n++) {
-          if (google.maps.geometry.spherical.computeDistanceBetween($scope.user.latLng, $scope.stations[n].latLng) < $scope.stations[n].radius && $scope.stations[n].playing === true) {
-            $scope.stationsInRange.pushIfNotExist($scope.stations[n], function(e) {
-                return e.name === $scope.stations[n].name;
-            });
-          } else if (google.maps.geometry.spherical.computeDistanceBetween($scope.user.latLng, $scope.stations[n].latLng) > $scope.stations[n].radius && $scope.stations[n].playing === true) {
-            var result = $scope.stationsInRange.filter(function(obj) {
-              return obj.name !== $scope.stations[n].name
-            });
-            $scope.stationsInRange = result;
-          }
-        }
-      }, 2000);
     }, function(error){
       console.log("Could not get location");
     });
@@ -214,6 +260,10 @@ angular.module('starter', ['ionic', 'ngCordova'])
       return obj.name == thisName;
     });
     $scope.playing = this.station;
-    document.getElementById('player').innerHTML = result[0].song;
+    document.getElementById('player').innerHTML = result[0].stream;
+  };
+
+  $scope.showThanks = function() {
+    document.querySelector('.create-station').innerHTML = 'Thank you for adding your station!<br>Your station will expire in 24 hours';
   };
 });
